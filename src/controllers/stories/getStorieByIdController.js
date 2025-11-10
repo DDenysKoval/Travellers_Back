545f1dd9ac91aff11a09ba003f0f7e381f0d49c6
juliya.lysenko <juliya.deleh@gmail.com>
2025-11-10 23:35:51 +0200

    getStoryId

--- src/controllers/stories/getStorieByIdController.js
+++ src/controllers/stories/getStorieByIdController.js
@@ -1,3 +1,19 @@
-const getContactByIdController = async (req, res) => {};
+import createHttpError from 'http-errors';
+import getStorieById from '../../services/stories/getStorieById.js';
 
-export default getContactByIdController;
+const getStorieByIdController = async (req, res) => {
+  const { storyId } = req.params;
+  const story = await getStorieById(storyId);
+
+  if (!story) {
+    throw new createHttpError.NotFound('Story not found');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Story found successfully',
+    data: story,
+  });
+};
+
+export default getStorieByIdController;
