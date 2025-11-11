e39b7f1051774bbd5ee01412ae8428b1ddf4bc73
Denys Koval <metalheadden@gmail.com>
2025-11-11 22:52:03 +0200

    db upd3

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -8,8 +8,13 @@ const initMongoConnection = async () => {
     const url = getEnvVar('MONGODB_URL');
     const db = getEnvVar('MONGODB_DB');
 
-    await mongoose.connect(`mongodb+srv://${user}:${pwd}@${url}/${db}`);
-
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log(
+      'MongoDB connected to database:',
+      mongoose.connection.db.databaseName,
+    );
     console.log('Mongo connection successfully established!');
   } catch (err) {
     console.log('Error while setting up mongo connection', err);
