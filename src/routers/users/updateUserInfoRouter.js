8375887510027e55d507f8e38cca56f174f967ee
Marina-Hizhnyak <maryfadeeva1208@gmail.com>
2025-11-11 16:57:44 +0100

    feat(users): add PATCH /users/update-me & GET /users/get-me + OpenAPI docs

--- src/routers/users/updateUserInfoRouter.js
+++ src/routers/users/updateUserInfoRouter.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../../utils/ctrlWrapper.js';
-import { isValidId } from '../../middlewares/isValidId.js';
 import { validateBody } from '../../middlewares/validateBody.js';
+import { authenticate } from '../../middlewares/authenticate.js';
+import updateUserInfoController from '../../controllers/users/updateUserInfoController.js';
+import { updateUserByIdSchema } from '../../validation/user/updateUserByIdSchema.js';
 
 const router = Router();
 
-router.patch('/:storieId', isValidId, validateBody(), ctrlWrapper());
+router.patch('/update-me', authenticate, validateBody(updateUserByIdSchema), ctrlWrapper(updateUserInfoController));
+
 
 export default router;
--- src/services/users/updateUserInfo.js
+++ src/services/users/updateUserInfo.js
@@ -1,3 +1,9 @@
-const updateUserInfo = async () => {};
+import { UsersCollection } from "../../db/models/users.js";
 
+const updateUserInfo = (userId, data = {}) => {
+  delete data.password;
+  return UsersCollection
+    .findByIdAndUpdate(userId, data, { new: true, runValidators: true })
+    .select('-password');
+};
 export default updateUserInfo;
