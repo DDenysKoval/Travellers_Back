3890f8e039973c4dad139a29ef8cd27ef97e25f3
Denys Koval <metalheadden@gmail.com>
2025-11-13 21:22:44 +0200

    cors fix

--- src/server.js
+++ src/server.js
@@ -14,7 +14,12 @@ const setupServer = () => {
   const app = express();
 
   app.use(express.json());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: ['http://localhost:3000', 'https://travellers-front.vercel.app/'],
+      credentials: true,
+    }),
+  );
   app.use(cookieParser());
   app.use(
     pino({
