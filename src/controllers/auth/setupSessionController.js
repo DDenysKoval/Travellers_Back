5aab98a976633f31e0aa25b04ec7cb0daa65defb
Denys Koval <metalheadden@gmail.com>
2025-11-15 17:28:35 +0200

    cookie fix

--- src/controllers/auth/setupSessionController.js
+++ src/controllers/auth/setupSessionController.js
@@ -1,18 +1,27 @@
-import { FIFTEEN_MINUTES, THIRTY_DAYS } from "../../constants/index.js";
+import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../../constants/index.js';
 
 const setupSessionController = (res, session) => {
-    res.cookie('accessToken', session.accessToken, {
-        httpOnly: true,
-        expires: new Date(Date.now() + FIFTEEN_MINUTES)
-    })
-    res.cookie('refreshToken', session.refreshToken, {
-        httpOnly: true,
-        expires: new Date(Date.now() + THIRTY_DAYS)
-    })
-    res.cookie('sessionId', session._id,{
-        httpOnly: true,
-        expires: new Date(Date.now() + THIRTY_DAYS)
-    })
+  res.cookie('accessToken', session.accessToken, {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'lax',
+    path: '/',
+    expires: new Date(Date.now() + FIFTEEN_MINUTES),
+  });
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'lax',
+    path: '/',
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'lax',
+    path: '/',
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
 };
 
 export default setupSessionController;
