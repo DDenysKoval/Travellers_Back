f10d1fb64878e8669e37e5428869131eaf66d1b3
Denys Koval <metalheadden@gmail.com>
2025-11-15 17:35:44 +0200

    cookie fix1

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const setupServer = () => {
       origin: ['http://localhost:3000', 'https://travellers-front.vercel.app'],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
       preflightContinue: false,
     }),
   );
