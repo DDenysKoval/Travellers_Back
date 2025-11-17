fad2d94a42bf27fbb03317fd77523c40e37bb660
Ann K <kubareva.ann@gmail.com>
2025-11-17 22:03:45 +0200

    setupSessionController fix

--- src/controllers/auth/setupSessionController.js
+++ src/controllers/auth/setupSessionController.js
@@ -1,18 +1,29 @@
-import { FIFTEEN_MINUTES, THIRTY_DAYS } from "../../constants/index.js";
+import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../../constants/index.js';
 
 const setupSessionController = (res, session) => {
-    res.cookie('accessToken', session.accessToken, {
-        httpOnly: true,
-        expires: new Date(Date.now() + FIFTEEN_MINUTES)
-    })
-    res.cookie('refreshToken', session.refreshToken, {
-        httpOnly: true,
-        expires: new Date(Date.now() + THIRTY_DAYS)
-    })
-    res.cookie('sessionId', session._id,{
-        httpOnly: true,
-        expires: new Date(Date.now() + THIRTY_DAYS)
-    })
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  const accessTokenOptions = {
+    httpOnly: true,
+    expires: new Date(Date.now() + FIFTEEN_MINUTES),
+    ...(isProduction ? { sameSite: 'none', secure: true } : {}),
+  };
+
+  const refreshTokenOptions = {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+    ...(isProduction ? { sameSite: 'none', secure: true } : {}),
+  };
+
+  const sessionIdOptions = {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+    ...(isProduction ? { sameSite: 'none', secure: true } : {}),
+  };
+
+  res.cookie('accessToken', session.accessToken, accessTokenOptions);
+  res.cookie('refreshToken', session.refreshToken, refreshTokenOptions);
+  res.cookie('sessionId', session._id, sessionIdOptions);
 };
 
 export default setupSessionController;
