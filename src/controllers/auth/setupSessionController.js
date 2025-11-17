c87553b940c67d68ccaf59a1094d9dfaa7f1fa86
Denys Koval <metalheadden@gmail.com>
2025-11-17 22:25:10 +0200

    sessions

--- src/controllers/auth/setupSessionController.js
+++ src/controllers/auth/setupSessionController.js
@@ -1,29 +1,18 @@
 import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../../constants/index.js';
 
 const setupSessionController = (res, session) => {
-  const isProduction = process.env.NODE_ENV === 'production';
-
-  const accessTokenOptions = {
+  res.cookie('accessToken', session.accessToken, {
     httpOnly: true,
     expires: new Date(Date.now() + FIFTEEN_MINUTES),
-    ...(isProduction ? { sameSite: 'none', secure: true } : {}),
-  };
-
-  const refreshTokenOptions = {
+  });
+  res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expires: new Date(Date.now() + THIRTY_DAYS),
-    ...(isProduction ? { sameSite: 'none', secure: true } : {}),
-  };
-
-  const sessionIdOptions = {
+  });
+  res.cookie('sessionId', session._id, {
     httpOnly: true,
     expires: new Date(Date.now() + THIRTY_DAYS),
-    ...(isProduction ? { sameSite: 'none', secure: true } : {}),
-  };
-
-  res.cookie('accessToken', session.accessToken, accessTokenOptions);
-  res.cookie('refreshToken', session.refreshToken, refreshTokenOptions);
-  res.cookie('sessionId', session._id, sessionIdOptions);
+  });
 };
 
 export default setupSessionController;
